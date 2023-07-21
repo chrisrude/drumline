d4a970a75f517fc2d6b4125ada27ba4cf0c348fe
Chris Rude <christopher.rude@gmail.com>
2023-07-21 11:17:34 -0700

    use typescript-eslint for linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,13 @@
 module.exports = {
+    parser: '@typescript-eslint/parser',
     root: true,
-    extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:svelte/recommended',
+        'prettier'
+    ],
+    plugins: ['@typescript-eslint'],
     parserOptions: {
         sourceType: 'module',
         ecmaVersion: 2020,

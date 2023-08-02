3c42b81e94cc7a56e0bf85874a1ba24d8761f010
Chris Rude <chrisrude@users.noreply.github.com>
2023-08-02 04:09:02 -0700

    remove express, don't need it

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,8 @@
 import process from 'node:process';
 
-import { server } from './express';
 import { EchoServer } from './websockets/';
 
-function onSocketError(err: Error) {
-    console.error(err);
-}
-
-const ws_server = new EchoServer({ noServer: true });
-
-server.on('upgrade', function upgrade(request, socket, head) {
-    socket.on('error', onSocketError);
-    ws_server.handleUpgrade(request, socket, head, function done(ws) {
-        ws_server.emit('connection', ws, request);
-    });
-});
+const ws_server = new EchoServer({ port: 8080 });
 
 process.on('exit', function exit() {
     ws_server.close();
